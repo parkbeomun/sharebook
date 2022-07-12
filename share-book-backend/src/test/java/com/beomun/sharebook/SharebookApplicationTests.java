a1c0cd532e0f34e5978986e6c635e6b2dbb887f2
parkbeomun <sos891214@gmail.com>
2022-07-12 22:49:00 +0900

    first commit

---
+++ share-book-backend/src/test/java/com/beomun/sharebook/SharebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beomun.sharebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
